1791d421d64fc00b0008f3a3ed75c16925ccd4ce
harrisonbui3 <harrisonbui3@gmail.com>
2017-07-10 09:51:58 -0700

    About Me (Assignment 1)

---
+++ About Me (Assignment 1)/src/about/me/assignment/pkg1/AboutMeAssignment1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package about.me.assignment.pkg1;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class AboutMeAssignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My name is Harrison Bui");
+        System.out.println("I am 16 years old");
+        System.out.println("I live in Fremont, California");
+        System.out.println("I love to play golf and play video games");
+        System.out.println("My favorite color is blue");
+    }
+    
+}
