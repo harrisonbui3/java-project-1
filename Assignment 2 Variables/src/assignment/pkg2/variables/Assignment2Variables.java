202ced834a97620e79aefa63cc9fab5fe565853b
harrisonbui3 <harrisonbui3@gmail.com>
2017-07-10 10:33:19 -0700

    Assignment 2 Variables

---
+++ Assignment 2 Variables/src/assignment/pkg2/variables/Assignment2Variables.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg2.variables;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Assignment2Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int age = 16;
+        double height = 70;
+        String firstName = "Harrison";
+        String lastName = "Bui";
+        boolean AStudent = true;
+        String hobbies = "play golf and video games";
+        String favanime = "Dragon Ball z";
+        
+        
+        
+        System.out.print("My name is " + firstName + " " + lastName + ". ");
+        System.out.println ("I am " + height + " inches tall and I am " + age + " years old.");
+        System.out.print ("My like to " + hobbies + ". ");
+        System.out.println ("My favorite anime is " + favanime);
+    } 
+    
+}
